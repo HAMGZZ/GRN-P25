472b514e7b7a688c6189550e9d109f232ddf90bc
hamgzz <vk2gzz@ieee.org>
2021-04-30 09:46:52 +1000

    Updated LCD settings

--- P25-Control.py
+++ P25-Control.py
@@ -15,18 +15,18 @@ from encoder import Encoder
 
 import Adafruit_CharLCD as LCD
 
-lcd_rs = 25
-lcd_en = 24
-lcd_d4 = 23
-lcd_d5 = 17
-lcd_d6 = 18
-lcd_d7 = 22
-lcd_backlight = 2
-lcd_columns = 16
-lcd_rows = 2
+lcd_rs = 27  # Change this to pin 21 on older revision Raspberry Pi's
+lcd_en = 22
+lcd_d4 = 25
+lcd_d5 = 24
+lcd_d6 = 23
+lcd_d7 = 18
+lcd_red   = 4
+lcd_green = 17
+lcd_blue  = 7  # Pin 7 is CE1
 
 
-lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_backlight)
+lcd = LCD.Adafruit_RGBCharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7, lcd_columns, lcd_rows, lcd_red, lcd_green, lcd_blue)
 
 
 
--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ sudo chmod 777 /tmp/ramdisk
 sudo mount -t tmpfs -o size=10m ramdisk /tmp/ramdisk
 touch /tmp/ramdisk/p25Data.gzz
 touch /tmp/ramdisk/p25DataSrc.gzz
-python P25-Control.py
+python3 P25-Control.py
