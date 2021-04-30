054a08fa43457c0ccd4e787f00d33f6ac0a9bb66
hamgzz <vk2gzz@ieee.org>
2021-05-01 08:27:43 +1000

    Killing OP25 works

--- P25-Control.py
+++ P25-Control.py
@@ -53,7 +53,7 @@ def signal_handler(sig, frame):
 
 
 def killp25():
-    os.kill(op25.pid, signal.SIGTERM)
+    os.kill(op25.pid, signal.SIGKILL)
 
 def file_len(fname):
     with open(fname) as f:
