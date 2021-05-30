91b263d96333f6bc6d944061c7fe029e6bbae992
Lewis Hamilton <32315974+HAMGZZ@users.noreply.github.com>
2021-05-30 17:38:58 +1000

    changed sample rate

--- startop25.sh
+++ startop25.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 rm /tmp/ramdisk/*
-./rx.py --args 'rtl' -N 'LNA:47' -S 57600 -x 2 -f 416.2875e6 -o 17e3 -d -200 -2 -T trunk.tsv -V -U -w -v 5 2> stderr-stream0.2 > /dev/null 
+./rx.py --args 'rtl' -N 'LNA:45' -S 97600 -x 2 -f 416.2875e6 -o 17e3 -d -200 -2 -T trunk.tsv -V -U -w 2> stderr-stream0.2 > /dev/null 
